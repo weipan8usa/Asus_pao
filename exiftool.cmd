5121e09bcd3ff626e7ec71bd14d0acad329bde54
Wei Pan <weipan8usa@gmail.com>
2021-04-27 23:22:42 -0400

    +youtube-dl.sh exiftool.cmd

--- exiftool.cmd
+++ exiftool.cmd
@@ -1,2 +1,5 @@
 exiftool -T -DateTimeOriginal -FileName Canon\ G5/*/*[Jj][Pp][Gg]
 exiftool -location:all VID_20161129_210121-p.mp4
+
+mkdir $(exiftool -T -CreateDate -FileName *|sed -e "s/:/-/g" |awk '{print $1}'|grep  "^[0-9]"|uniq)
+exiftool -T -CreateDate -FileName *|sed -e "s/:/-/g" |grep "^[0-9]" |awk '{print "mv -i ",$NF,$1}'> /tmp/d
--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,3 +1,4 @@
 youtube-dl --list-subs URL
 youtube-dl --write-sub --sub-lang zh-TW  --skip-download https://www.youtube.com/watch?v=vCcjZusjY-s
 youtube-dl --list-subs https://www.youtube.com/watch?v=Ye8mB6VsUHw
+#youtube-dl -f 18 https://www.youtube.com/watch?v=pz1NL8zJHQU --output 血小板.mp4
