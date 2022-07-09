36e8ee217abe4a8913fc294e067549b50ba26117
Wei Pan <weipan8usa@gmail.com>
2022-07-09 15:11:06 -0400

    youtube-dl.sh

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -2,3 +2,4 @@ youtube-dl --list-subs URL
 youtube-dl --write-sub --sub-lang zh-TW  --skip-download https://www.youtube.com/watch?v=vCcjZusjY-s
 youtube-dl --list-subs https://www.youtube.com/watch?v=Ye8mB6VsUHw
 #youtube-dl -f 18 https://www.youtube.com/watch?v=pz1NL8zJHQU --output 血小板.mp4
+youtube-dl --list-thumbnails https://youtu.be/BPMJuRrN-Nc
