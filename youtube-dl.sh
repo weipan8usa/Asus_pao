d4bc2046df1b49c3b65be6102567707f28fb356f
Wei Pan <weipan8usa@gmail.com>
2022-08-13 17:43:31 -0400

    updates

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -3,3 +3,5 @@ youtube-dl --write-sub --sub-lang zh-TW  --skip-download https://www.youtube.com
 youtube-dl --list-subs https://www.youtube.com/watch?v=Ye8mB6VsUHw
 #youtube-dl -f 18 https://www.youtube.com/watch?v=pz1NL8zJHQU --output 血小板.mp4
 youtube-dl --list-thumbnails https://youtu.be/BPMJuRrN-Nc
+#youtube-dl  --get-thumbnail https://youtu.be/BPMJuRrN-Nc|wget -i -
+
